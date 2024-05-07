c20c7589f1082c3e769d9849738c0f8e976ec2fb
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-05-08 00:37:08 +0300

    added all functions for REST APİ
    CRUD operations

--- runnerz/src/main/java/com/bartugkaan/runnerz/run/RunController.java
+++ runnerz/src/main/java/com/bartugkaan/runnerz/run/RunController.java
@@ -1,16 +1,55 @@
 package com.bartugkaan.runnerz.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository){
+        this.runRepository = runRepository;
+    }
+
+    @GetMapping("")
+    List<Run> findAll(){
+        return runRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id){
+       Optional<Run> run = runRepository.findById(id);
+       if (run.isEmpty()){
+           throw  new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+       }
+       return run.get();
+    }
+
+    //Post
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    void create(@RequestBody Run run){
+        runRepository.create(run);
+    }
 
 
+    //put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run,@PathVariable Integer id){
+        runRepository.update(run, id);
+    }
 
-    @GetMapping("/hello")
-    String home() {
-        return "Welcome to Runnerz!";
+    //Delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id){
+        runRepository.delete(id);
     }
 
 }
